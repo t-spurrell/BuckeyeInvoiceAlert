9c8a12eb2654e30450313a23cdda912856861652
Trevor <jaguarxman@gmail.com>
2023-03-01 20:52:52 -0500

    added fuction to check if its the first of the month, if so delete the notified text file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import smtplib
 import notes
+import os
+import pandas as pd
 from email.message import EmailMessage
 from configuration import load_config, file_exist
 from invoice_ninja import InvoiceNinja
@@ -10,7 +12,7 @@ CONFIG = load_config()
 invoice_ninja_conn = InvoiceNinja(host=CONFIG['invoice_ninja']['base_url'], token=CONFIG['invoice_ninja']['token'])
 
 
-def sendEmail(body=None):
+def send_email(body=None):
     pw = CONFIG['email']['pw']
     msg = EmailMessage()
     msg['Subject'] = f'test'
@@ -36,7 +38,16 @@ def previously_notified(invoice_id):
                 return False
 
 
+def first_of_month():
+    day = pd.Timestamp.today()
+    if day.is_month_start:
+        if file_exist('notified'):
+            os.remove('notified')
+
+
 def main():
+    first_of_month()
+
     overdue = []
     ninja_overdue = invoice_ninja_conn.get_overdue_invoices()
 
@@ -47,7 +58,6 @@ def main():
                 client_id = o['client_id']
                 client_name = invoice_ninja_conn.get_client_name(client_id)
                 balance = o['balance']
-                print(f'Client: {client_name} with invoice number {invoice_id} has a past due amount of ${balance}')
                 with open('notified', 'a') as f:
                     f.write(str(invoice_id) + '\n')
                 client = {
@@ -57,8 +67,8 @@ def main():
                 }
                 overdue.append(client)
         if overdue:
-            print('send email')
             body = notes.overdue(overdue)
+            #send_email(body)
             print(body)
         else:
             print('no overdue clients')
@@ -66,6 +76,8 @@ def main():
 
 if __name__ == '__main__':
     main()
-    #sendEmail()
+    #send_email()
+
+
 
 
